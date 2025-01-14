c1450b8380cb815284a9e49ea6395c856d6ec467
Ines Amirat <inesamirat20@gmail.com>
2025-01-14 23:55:40 +0100

    initial commit

---
+++ Prayer-times.js
@@ -0,0 +1,39 @@
+/*function userPosts(IdUser) {
+
+
+    url = `https://jsonplaceholder.typicode.com/posts?userId=${IdUser}`
+    axios.get(url)
+        .then((response) => {
+            console.log(response)
+            let posts = response.data
+            document.getElementById("postI").innerHTML = ""
+            for (post of posts) {
+                document.getElementById("postI").innerHTML += ` <div class="postUser">
+                                                                <h1>${post.title}</h1>
+                                                                <h3>${post.body}</h3>
+                                                            </div>`
+            }
+
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+}
+        
+
+userPosts(1)
+*/
+
+
+function prayerTimeCity(){
+       let address= "annaba"
+       url = `https://api.allorigins.win/raw?url=https://api.aladhan.com/timingsByAddress/02-01-2025?address=${address}`
+       axios.get(url)
+             .then((response) => {
+                  let data = response.data
+                  console.log(data.data.timings)
+             })
+}
+
+prayerTimeCity()
