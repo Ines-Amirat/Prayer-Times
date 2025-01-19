03a1d34206aef5fa60cd96adcb16d3f7a978145d
Ines Amirat <inesamirat20@gmail.com>
2025-01-19 20:53:39 +0100

    m

--- Prayer-times.js
+++ Prayer-times.js
@@ -1,20 +1,29 @@
 
 function prayerTimeCity() {
-   
-    let address = document.getElementById("city").value
-    console.log(document.getElementById("city").value)
-    url = `https://api.allorigins.win/raw?url=https://api.aladhan.com/timingsByAddress/16-01-2025?address=${address}`
+
+    document.getElementById("table-prayer").innerHTML = `<tr>
+                                                            <th class="th">Prayer</th>
+                                                            <th class="th">Azan Time</th>
+                                                        </tr>`
+    let address = document.getElementById("placeEnter").value
+    document.getElementById("placeEnter").value=""
+    url = `https://api.allorigins.win/raw?url=https://api.aladhan.com/timingsByAddress?address=${address}`
+
+     /* axios.get(url, {
+          params: params
+
+    })  */
     axios.get(url)
         .then((response) => {
             let data = response.data
             let timings = data.data.timings
              
-
+           
             let gregorian = data.data.date.gregorian.date
             let hijri = data.data.date.hijri.date
             let method = data.data.meta.method.name
 
-            document.getElementById("city").innerHTML = `Prayer Times in ${address},`
+            document.getElementById("city").innerHTML = `Prayer Times in ${address}`
             document.getElementById("miladi").innerHTML = gregorian
             document.getElementById("hijri").innerHTML = hijri
             Object.getOwnPropertyNames(timings).forEach((val) => {
@@ -23,7 +32,6 @@ function prayerTimeCity() {
                                                                                 <td>${timings[val]}</td>   
                                                                         </tr>`
                                                                     })
-            console.log( Object.getOwnPropertyNames(timings))
           
             document.getElementsByClassName("method")[0].innerHTML = method
             })
